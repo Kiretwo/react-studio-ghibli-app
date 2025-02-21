5346982be4617e638ac26c69b3c7a18382fec03f
Kiretwo <eritor00991@stud.noroff.no>
2025-02-21 14:03:29 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tailwindPostcss from "@tailwindcss/postcss";
+export default defineConfig({
+  plugins: [
+    react(), // React plugin for Vite
+    tailwindcss(), // Tailwind CSS plugin for Vite
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindPostcss, // Use the new PostCSS plugin
+      ],
+    },
+  },
+  server: {
+    port: 1183, // Set the server port to 1183
+  },
+});
